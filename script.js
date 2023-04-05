defbae8509591e605f2ea076a2fa942a681b6cc3
Dom Brady <dom.brades@gmail.com>
2023-04-05 16:42:05 +0100

    Added functionaility

---
+++ script.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.querySelector('.item-description-div').style.display = 'flex';
+    
+  });
+  
+  item.addEventListener('mouseout', () => {
+    item.querySelector('.item-description-div').style.display = 'none';
+    
+  });
+});
