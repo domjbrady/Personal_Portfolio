66e3a9e2c3433679550e54ca5e6caca2634825ea
Dom Brady <dom.brades@gmail.com>
2023-04-09 12:36:30 +0100

    Add Anchor Tags and more styling

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-/*const items = document.querySelectorAll('.item');
-
-items.forEach(item => {
-  item.addEventListener('mouseover', () => {
-    item.querySelector('.item-description-div').style.opacity = '0';
-    
-  });
-  
-  item.addEventListener('mouseout', () => {
-    item.querySelector('.item-description-div').style.opacity = '1';
-    
-  });
 
 
-});
-
-*/
+  function uncheckCheckbox() {
+    document.getElementById('check').checked = false;
+  }
